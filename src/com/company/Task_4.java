80e6d0cf7cd4e5f5be152d102f9d3ed3bbcaf1f1
Mukucha <100211654+Mukucha@users.noreply.github.com>
2022-05-03 20:00:23 +0200

    Initial commit

---
+++ src/com/company/Task_4.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Task_4 {
+    public static void main(String[] args) throws IOException{
+
+        DataManagement dm = new DataManagement();
+        dm.ReadData();
+        dm.WriteToFile();
+        System.out.println("\n---------\n");
+        dm.ReadFromFile();
+
+    }
+}
